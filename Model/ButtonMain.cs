0db9b1c4dff986af00d5d5b982ac31fef7a76681
Максим Лузан <maks7ronaldo@gmail.com>
2023-04-12 15:52:33 +0300

    Добавлены основные кнопки меню

--- Model/ButtonMain.cs
+++ Model/ButtonMain.cs
@@ -15,6 +15,20 @@ namespace DeliveryApplication.Model
         public ButtonMain()
         {
             InitializeComponent();
+
+            int padding = 16;
+            int workSize = (Screen.PrimaryScreen.Bounds.Width - padding * 5);
+            int widthSquare = btnSaleBoxes.Height;
+            int width = (workSize - (widthSquare * 2)) / 2;
+            btnAcceptParcel.Width = width;
+            btnGiveParcel.Width = width;
+            btnSaleBoxes.Width = widthSquare;
+            guna2Button4.Width = widthSquare;
+
+            btnAcceptParcel.Location = new Point(padding, padding);
+            btnGiveParcel.Location = new Point(padding * 2 + width, padding);
+            btnSaleBoxes.Location = new Point(padding * 3 + width * 2, padding);
+            guna2Button4.Location = new Point(padding * 4 + width * 2 + widthSquare, padding);
         }
 
         private void ButtonMain_Load(object sender, EventArgs e)
